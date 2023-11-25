3122c19c866bb73a8d05165a6eb7ccd07a1c7f24
Daniel <cr9vercloud@gmail.com>
2023-11-25 02:38:35 +0100

    made final fix for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t cr9ver/multi-client:latest -t cr9ver/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t cr9ver/multi-server:latest -t cr9ver/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t cr9ver/multi-worker:latest -t cr9ver/multi-worker:$SHA -f ./server/Dockerfile ./worker
+docker build -t cr9ver/multi-worker:latest -t cr9ver/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push cr9ver/multi-client:latest
 docker push cr9ver/multi-server:latest
 docker push cr9ver/multi-worker:latest
