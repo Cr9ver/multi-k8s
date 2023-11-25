3d65bbefd3711a81fa9bae67cc3ad4a84ee3d3aa
Daniel <cr9vercloud@gmail.com>
2023-11-25 01:40:54 +0100

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t cr9ver/multi-client:latest -t cr9ver/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t cr9ver/multi-server:latest -t cr9ver/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t cr9ver/multi-worker:latest -t cr9ver/multi-worker:$SHA ./server/Dockerfile ./worker
+docker push cr9ver/multi-client:latest
+docker push cr9ver/multi-server:latest
+docker push cr9ver/multi-worker:latest
+
+
+docker push cr9ver/multi-client:$SHA
+docker push cr9ver/multi-server:$SHA
+docker push cr9ver/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=cr9ver/multi-server:$SHA
+kubectl set image deployments/client-deployment client=cr9ver/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=cr9ver/multi-worker:$SHA
